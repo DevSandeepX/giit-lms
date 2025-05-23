37da2deaf9ac408d5cab6cfa10bfb6e014c8d228
DevSandeepX <sandeep87565590@gmail.com>
2025-05-23 11:52:16 +0530

    // Description: This script is used to fix a warning in MongoDB about the use of deprecated options // Description: Fixes a warning in MongoDB about the use of deprecated options

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = ()=>{
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+    mongoose.connect(process.env.MONGO_URI)
       .then(() => console.log('✅ MongoDB connected'))
       .catch(err => console.error('❌ MongoDB connection error:', err));
 }
